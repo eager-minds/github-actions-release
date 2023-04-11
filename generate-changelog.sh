17655c3698a0243e23daf2e72551e393e9164f89
Javier de Diego Navarro <javier@eagerminds.tech>
2023-04-11 21:54:28 +0200

    Updated

--- generate-changelog.sh
+++ generate-changelog.sh
@@ -1,39 +1,28 @@
 #!/bin/sh
 
-echo "env.TAG_NAME: $TAG_NAME"
-echo "env.CHANGELOG_PATH: $CHANGELOG_PATH"
-
 if [ -z "$TAG_NAME" ]; then
-echo "Please provide the version number as an argument."
-exit 1
+  echo "Please provide the version number as an argument."
+  exit 1
 fi
 
 if [ ! -f "$CHANGELOG_PATH" ]; then
-echo "$CHANGELOG_PATH file not found in the current directory."
-exit 1
+  echo "$CHANGELOG_PATH file not found in the current directory."
+  exit 1
 fi
 
 START_LINE=$(grep -n "^## \[$TAG_NAME\]" "$CHANGELOG_PATH" | cut -d : -f 1)
 
 if [ -z "$START_LINE" ]; then
-echo "The provided version \"$TAG_NAME\" is not found in the CHANGELOG file: $CHANGELOG_PATH"
-exit 1
+  echo "The provided version \"$TAG_NAME\" is not found in the CHANGELOG file: $CHANGELOG_PATH"
+  exit 1
 fi
 
 NEXT_VERSION_LINE=$(tail -n +$((START_LINE + 1)) "$CHANGELOG_PATH" | grep -n -m 1 "^## \[" | cut -d : -f 1)
 
 if [ -z "$NEXT_VERSION_LINE" ]; then
-TAG_MESSAGE=$(tail -n +"$START_LINE" "$CHANGELOG_PATH")
+  TAG_MESSAGE=$(tail -n +"$START_LINE" "$CHANGELOG_PATH")
 else
-TAG_MESSAGE=$(tail -n +"$START_LINE" "$CHANGELOG_PATH" | head -n "$((NEXT_VERSION_LINE))")
+  TAG_MESSAGE=$(tail -n +"$START_LINE" "$CHANGELOG_PATH" | head -n "$((NEXT_VERSION_LINE))")
 fi
 
-echo "env.TAG_NAME: $TAG_NAME"
-echo "env.CHANGELOG_PATH: $CHANGELOG_PATH"
-echo "env.START_LINE: $START_LINE"
-echo "env.NEXT_VERSION_LINE: $NEXT_VERSION_LINE"
-echo "env.TAG_MESSAGE: $TAG_MESSAGE"
-
-rel=$(gh release create "$TAG_NAME" -t "$RELEASE_NAME" --notes "${TAG_MESSAGE}")
-
-echo "Rel: $rel"
+echo "$TAG_MESSAGE"
